fb190bfead475d2c952e06642a56d0eb079e9320
Angel Lopez <a.o.lopez.cruz@icloud.com>
2024-05-25 14:17:20 -0600

    tablas segunda parte

--- TableViewSegundaParte/ViewController.swift
+++ TableViewSegundaParte/ViewController.swift
@@ -8,12 +8,63 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var  searchBar: UISearchBar!
+    
+    var myArray = ["Pedro", "Juan", "Maria", "Jose", "Mariana", "Dante", "Isabel", "Victoria"]
+    var searchArray: [String] = []
+    // esta variable nos va a servir para logica de "buscar"
+    var searching = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        self.tableView.dataSource = self
+        self.searchBar.delegate = self
     }
+    
+    @IBAction func editTable () {
+        if tableView.isEditing {
+            tableView.isEditing = false
+        } else {
+            tableView.isEditing = true
+        }
+    }
+}
 
+extension ViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        true
+    }
+    
+    // los dos incidices nos ayudan a saber el indice en el que se esta y el indice al que
+    // va a llegar
+    func tableView(_ tableView: UITableView, moveRowAt sourceIndexPath: IndexPath, to destinationIndexPath: IndexPath) {
+        // esta funcion swapAt le pasas dos posiciones y te las intercambia
+        myArray.swapAt(sourceIndexPath.row, destinationIndexPath.row)
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if searching {
+            searchArray.count
+        } else {
+            myArray.count
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell")
+        cell?.textLabel?.text = myArray[indexPath.row]
+        
+        return cell!
+    }
+}
 
+extension ViewController: UISearchBarDelegate {
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        searchArray = myArray.filter({ $0.prefix(searchText.count) == searchText })
+            searching = true
+        print(searchArray)
+    }
 }
 
